a8663d3e38b669e3f914a101706956a31b24bed8
cmshalom <cmshalom@cs.technion.ac.il>
2019-07-11 10:39:54 +0300

    Some fixes to main

--- DataStructuresBU/main.cpp
+++ DataStructuresBU/main.cpp
@@ -14,25 +14,29 @@ extern int fibonacciWithMemoization(int n, int &steps);        // computes recur
 extern int fibonacciWithDynamicProgramming(int n, int &steps); // computes using dynamic programming
 
 void print (string algorithm, int n, int result, int steps) {
-	cout << algorithm << ":" << "F[" << n << "]=" << result << ", computed in " << steps << steps;
+	cout << algorithm << ":" << "F[" << n << "]=" << result << ", computed in " << steps << " steps" << endl;
 }
 
 int main (int argc, char **argv) {
 
-	int steps;
+	int steps=0;
 	int f = fibonacciRecursive(10, steps);
 	print("Recursive", 10, f, steps);
 
+	steps=0;
 	f = fibonacciWithMemoization(10, steps);
 	print("Recursive with Memoization", 10, f, steps);
 
+	steps=0;
 	f = fibonacciWithDynamicProgramming(10, steps);
 	print("Dynamic Programming", 10, f, steps);
 
-	f = fibonacciWithMemoization(100, steps);
-	print("Recursive with Memoization", 100, f, steps);
+	steps=0;
+	f = fibonacciWithMemoization(40, steps);
+	print("Recursive with Memoization", 40, f, steps);
 
-	f = fibonacciWithDynamicProgramming(100, steps);
-	print("Dynamic Programming", 100, f, steps);
+	steps=0;
+	f = fibonacciWithDynamicProgramming(40, steps);
+	print("Dynamic Programming", 40, f, steps);
 
 }
